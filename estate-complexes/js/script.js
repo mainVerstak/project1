e917d7476af54cda904136c82cec0a80ae085f94
Diman <pavlenkodim@mail.ru>
2024-09-20 00:40:24 +0500

    add canvas chart

--- estate-complexes/js/script.js
+++ estate-complexes/js/script.js
@@ -57,6 +57,15 @@ $(function () {
     $(".ds-chart__tab").removeClass("active").eq(tabIdx).addClass("active");
   });
 
+  $(document).ready(function () {
+    var tabIdx = $(".ds-chart__tabs-list li").index();
+    $(".ds-chart__tabs-list li")
+      .addClass("active")
+      .siblings()
+      .removeClass("active");
+    $(".ds-chart__tab").removeClass("active").eq(tabIdx).addClass("active");
+  });
+
   const ctx = document.getElementById("barChart");
 
   new Chart(ctx, {
@@ -88,14 +97,98 @@ $(function () {
   const ctx1 = document.getElementById("lineChart");
 
   new Chart(ctx1, {
-    type: "bar",
     data: {
-      labels: ["авг 24", "дек 23", "апр 24"],
+      labels: [
+        "сен 2023",
+        "окт 2023",
+        "ноя 2023",
+        "дек 2023",
+        "янв 2024",
+        "фев 2024",
+        "мар 2024",
+        "апр 2024",
+      ],
       datasets: [
         {
-          label: "цена $",
-          data: [1.1, 1, 2],
+          type: "line",
+          label: "- Средняя цена за 1 м2, руб.",
+          data: [7, 9, 8, 8, 6, 10, 6, 7, 5],
+          backgroundColor: "#ffffff",
           borderWidth: 1,
+          borderColor: "#000000",
+        },
+        {
+          type: "bar",
+          label: "- Колличество проданных квартир",
+          data: [5, 7, 6, 6, 4, 8, 4, 5, 3],
+          backgroundColor: "#2D6BA1",
+          borderWidth: 1,
+          borderRadius: 5,
+        },
+      ],
+    },
+    options: {
+      elements: {
+        line: { tension: 0.4 },
+        bar: { borederRadius: 5 },
+      },
+      plugins: {
+        legend: {
+          display: true,
+          position: "bottom",
+          align: "start",
+          labels: {
+            usePointStyle: true,
+          },
+        },
+      },
+      scales: {
+        y: {
+          beginAtZero: true,
+          grid: { display: true },
+          ticks: {
+            callback: function (value, index, ticks) {
+              return "";
+            },
+          },
+        },
+        x: { grid: { display: false } },
+      },
+    },
+  });
+
+  const cirklAppart = document.getElementById("appartCirklChart");
+
+  new Chart(cirklAppart, {
+    type: "doughnut",
+    data: {
+      datasets: [
+        {
+          data: [33, 77],
+          backgroundColor: "#2D6BA1",
+        },
+      ],
+    },
+    options: {
+      plugins: {
+        title: {
+          display: false,
+        },
+        legend: { display: true },
+      },
+      scales: {},
+    },
+  });
+
+  const nonResCirkl = document.getElementById("nonResidentCirklChart");
+
+  new Chart(nonResCirkl, {
+    type: "doughnut",
+    data: {
+      datasets: [
+        {
+          data: [33, 77],
+          backgroundColor: "#2D6BA1",
         },
       ],
     },
@@ -105,11 +198,29 @@ $(function () {
           display: false,
         },
       },
-      scales: {
-        y: {
-          beginAtZero: true,
+      scales: {},
+    },
+  });
+
+  const parkingCirkl = document.getElementById("parkingCirklChart");
+
+  new Chart(parkingCirkl, {
+    type: "doughnut",
+    data: {
+      datasets: [
+        {
+          data: [33, 77],
+          backgroundColor: "#2D6BA1",
+        },
+      ],
+    },
+    options: {
+      plugins: {
+        title: {
+          display: false,
         },
       },
+      scales: {},
     },
   });
 
