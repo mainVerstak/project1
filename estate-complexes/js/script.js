fa6225f0019ccb85df90e46dbf38536c7d0225c1
Diman <pavlenkodim@mail.ru>
2024-10-18 01:46:18 +0500

    fix chart

--- estate-complexes/js/script.js
+++ estate-complexes/js/script.js
@@ -150,11 +150,6 @@ $(function () {
         responsive: true,
         aspectRatio: 2,
         maintainAspectRatio: false,
-        onResize: (chart, size) => {
-          console.log("chart", chart);
-          console.log("size", size);
-          chart.resize();
-        },
         interaction: {
           mode: "index",
           intersect: false,
