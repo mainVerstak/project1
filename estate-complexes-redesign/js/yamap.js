1a81d3200839171667110cc0f908734df907f368
Diman <pavlenkodim@mail.ru>
2024-11-08 16:05:47 +0500

    add styles for cluster in yamap

--- estate-complexes-redesign/js/yamap.js
+++ estate-complexes-redesign/js/yamap.js
@@ -76,7 +76,7 @@ async function initMap() {
         className = "medical";
         content = `
           <svg class="map-icon" >
-            <use xlink:href="./map-icons/map-icons.svg#buildings"></use>
+            <use xlink:href="./map-icons/map-icons.svg#plus"></use>
           </svg>`;
         break;
       case "Спорт":
@@ -116,40 +116,135 @@ async function initMap() {
   }
 
   // Функция создания DOM элемента для кластера
-  function createClusterElement(count) {
+  function createClusterElement(count, data) {
+    let className;
+    console.log(data);
     const element = document.createElement("div");
+    switch (data[0].properties.tabName) {
+      case "Развлечения":
+        className = "entertainment";
+        break;
+      case "Образование":
+        className = "study";
+        break;
+      case "Медицина":
+        className = "medical";
+        break;
+      case "Спорт":
+        className = "sport";
+        break;
+      case "Продукты":
+        className = "market";
+        break;
+      case "Новостройки":
+        className = "new-building";
+        break;
+      default:
+        className = "default-building";
+    }
     element.innerHTML = `
       <div class="cluster-marker">
-        <div class="cluster-marker__inner">${count}</div>
+        <div class="cluster-marker__inner ${className}">${count}</div>
       </div>
     `;
     return element.firstElementChild;
   }
 
-  // Создаем кластеризатор
-  const clusterer = new YMapClusterer({
-    method: clusterByGrid({ gridSize: 64 }),
-    features: points,
-    marker: (feature) => {
-      return new YMapMarker(
-        {
-          coordinates: feature.geometry.coordinates,
-        },
-        createMarkerElement(feature.properties)
-      );
-    },
-    cluster: (coordinates, features) => {
-      return new YMapMarker(
-        {
-          coordinates: coordinates,
-        },
-        createClusterElement(features.length)
-      );
-    },
+  // Группируем точки по категориям
+  const groupedPoints = {
+    entertainment: [],
+    study: [],
+    medical: [],
+    sport: [],
+    market: [],
+    newBuilding: [],
+    default: [],
+  };
+
+  points.forEach((point) => {
+    console.log(point.properties.tabName);
+    let tabName;
+
+    switch (point.properties.tabName) {
+      case "Развлечения":
+        tabName = "entertainment";
+        break;
+      case "Образование":
+        tabName = "study";
+        break;
+      case "Медицина":
+        tabName = "medical";
+        break;
+      case "Спорт":
+        tabName = "sport";
+        break;
+      case "Продукты":
+        tabName = "market";
+        break;
+      case "Новостройки":
+        tabName = "newBuilding";
+        break;
+      default:
+        tabName = "default";
+    }
+
+    if (groupedPoints[tabName]) {
+      groupedPoints[tabName].push(point);
+    } else {
+      groupedPoints.default.push(point);
+    }
   });
 
-  // Добавляем кластеризатор на карту
-  map.addChild(clusterer);
+  // Создаем кластеризатор для каждой группы
+  Object.keys(groupedPoints).forEach((category) => {
+    if (groupedPoints[category].length > 0) {
+      const clusterer = new YMapClusterer({
+        method: clusterByGrid({ gridSize: 64 }),
+        features: groupedPoints[category],
+        marker: (feature) => {
+          return new YMapMarker(
+            {
+              coordinates: feature.geometry.coordinates,
+            },
+            createMarkerElement(feature.properties)
+          );
+        },
+        cluster: (coordinates, features) => {
+          return new YMapMarker(
+            {
+              coordinates: coordinates,
+            },
+            createClusterElement(features.length, features)
+          );
+        },
+      });
+
+      // Создаем кластеризатор
+      // const clusterer = new YMapClusterer({
+      //   method: clusterByGrid({ gridSize: 64 }),
+      //   features: points,
+      //   marker: (feature) => {
+      //     return new YMapMarker(
+      //       {
+      //         coordinates: feature.geometry.coordinates,
+      //       },
+      //       createMarkerElement(feature.properties)
+      //     );
+      //   },
+      //   cluster: (coordinates, features) => {
+      //     return new YMapMarker(
+      //       {
+      //         coordinates: coordinates,
+      //       },
+      //       createClusterElement(features.length)
+      //     );
+      //   },
+      // });
+
+      // Добавляем кластеризатор на карту
+      map.addChild(clusterer);
+    }
+  });
 }
 
 initMap();
