0ccd3621f63fed8fdc0a0226b75b5f39cfda18d0
Dmitry Pavlenko <pavlenkodim@mail.ru>
2024-11-08 14:04:59 +0500

    add map markers

--- estate-complexes-redesign/js/yamap.js
+++ estate-complexes-redesign/js/yamap.js
@@ -1,5 +1,17 @@
 const ymaps3 = window.ymaps3;
 
+async function getMapData() {
+  try {
+    const response = await fetch("./js/map-data.json").then((res) =>
+      res.json()
+    );
+    console.log(response.data); // TODO: удалить
+    return response.data;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 async function initMap() {
   await ymaps3.ready;
 
@@ -12,37 +24,93 @@ async function initMap() {
 
   const map = new YMap(document.getElementById("map"), {
     location: {
-      center: [37.588144, 55.733842],
-      zoom: 10,
+      center: [39.710368, 47.222263],
+      zoom: 15,
     },
   });
 
   map.addChild(new YMapDefaultSchemeLayer());
   map.addChild(new YMapDefaultFeaturesLayer());
 
-  // Генерируем тестовые данные
-  const features = Array.from({ length: 100 }, (_, index) => ({
-    type: "Feature",
-    id: index,
-    geometry: {
-      type: "Point",
-      coordinates: [
-        37.588144 + (Math.random() - 0.5) * 0.2,
-        55.733842 + (Math.random() - 0.5) * 0.2,
-      ],
-    },
-    properties: {
-      balloonContent: `Объект ${index + 1}`,
-      clusterCaption: `Объект ${index + 1}`,
-      price: Math.floor(Math.random() * 50000 + 30000),
-    },
-  }));
+  // Получаем тестовые данные и преобразуем в точки
+  const data = await getMapData();
+  const points = data.map(
+    ({ latitude, longitude, address, tabName, name }, index) => ({
+      type: "Feature",
+      id: index,
+      geometry: {
+        type: "Point",
+        coordinates: [longitude, latitude],
+      },
+      properties: {
+        balloonContent: `Объект ${index + 1}`,
+        clusterCaption: `Объект ${index + 1}`,
+        address: address,
+        tabName: tabName,
+        name: name,
+      },
+    })
+  );
 
   // Функция создания DOM элемента для маркера
-  function createMarkerElement(content) {
+  function createMarkerElement(data) {
+    let className;
+    let content;
+
+    switch (data.tabName) {
+      case "Развлечения":
+        className = "entertainment";
+        content = `
+          <svg class="map-icon" >
+            <use xlink:href="./map-icons/map-icons.svg#video"></use>
+          </svg>`;
+        break;
+      case "Образование":
+        className = "study";
+        content = `
+          <svg class="map-icon" >
+            <use xlink:href="./map-icons/map-icons.svg#teacher"></use>
+          </svg>`;
+        break;
+      case "Медицина":
+        className = "medical";
+        content = `
+          <svg class="map-icon" >
+            <use xlink:href="./map-icons/map-icons.svg#buildings"></use>
+          </svg>`;
+        break;
+      case "Спорт":
+        className = "sport";
+        content = `
+          <svg class="map-icon" >
+            <use xlink:href="./map-icons/map-icons.svg#cup"></use>
+          </svg>`;
+        break;
+      case "Продукты":
+        className = "market";
+        content = `
+          <svg class="map-icon" >
+            <use xlink:href="./map-icons/map-icons.svg#shop"></use>
+          </svg>`;
+        break;
+      case "Новостройки":
+        className = "new-building";
+        content = `
+          <svg class="map-icon" >
+            <use xlink:href="./map-icons/map-icons.svg#buildings"></use>
+          </svg>`;
+        break;
+      default:
+        className = "default-building";
+        content = `
+          <svg class="map-icon" >
+            <use xlink:href="./map-icons/map-icons.svg#buildings"></use>
+          </svg>`;
+    }
+
     const element = document.createElement("div");
     element.innerHTML = `
-      <div class="single-marker"></div>
+      <div class="single-marker ${className}">${content}</div>
     `;
     return element.firstElementChild;
   }
@@ -61,13 +129,13 @@ async function initMap() {
   // Создаем кластеризатор
   const clusterer = new YMapClusterer({
     method: clusterByGrid({ gridSize: 64 }),
-    features: features,
+    features: points,
     marker: (feature) => {
       return new YMapMarker(
         {
           coordinates: feature.geometry.coordinates,
         },
-        createMarkerElement()
+        createMarkerElement(feature.properties)
       );
     },
     cluster: (coordinates, features) => {
